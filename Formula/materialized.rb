5295af80e249cf6a7e0d7a165add5bd22789fbb0
Brennan Vincent <brennan@materialize.io>
2020-04-11 19:02:45 -0400

    v0.2.0

--- Formula/materialized.rb
+++ Formula/materialized.rb
@@ -1,19 +1,19 @@
 class Materialized < Formula
   desc "The streaming data warehouse"
   homepage "https://materialize.io/docs/"
-  url "https://github.com/MaterializeInc/materialize/archive/v0.1.3.tar.gz"
-  sha256 "23aa89b464a69cb143372adbd7efe6e04fe5982d0964d357a9d9890854a2c66e"
+  url "https://github.com/MaterializeInc/materialize/archive/v0.2.0.tar.gz"
+  sha256 "d906f7e972756b0459685492be8788e9580206928ce980e28c7313adc4011dbc"
   head "https://github.com/MaterializeInc/materialize.git"
 
   bottle do
     root_url "https://packages.materialize.io/homebrew"
-    sha256 "84f9b4c05680e3e84f0f1f0af6a0dafc293624f3cccee8faabb7e95096dfa8a9" => :high_sierra
+    sha256 "fca1511d6a380fa12ae4d51767b6f077e1569d409ea4e3328c5ff765a50661e9" => :high_sierra
   end
 
   depends_on "cmake" => :build
   depends_on "rust" => :build
 
-  STABLE_BUILD_SHA = "ac35436462038fbac2b65f6ca020553a4cbef60d".freeze
+  STABLE_BUILD_SHA = "35b4fc23ec726baf0a293d60890ac936e03971f1".freeze
 
   def build_sha
     if head?
