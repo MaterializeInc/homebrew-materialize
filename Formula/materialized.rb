e683500523c0395e97f0be00de7adff0b9574b56
Brandon W Maister <bwm@materialize.io>
2021-06-09 13:13:09 -0400

    Release v0.8.0 (#44)

--- Formula/materialized.rb
+++ Formula/materialized.rb
@@ -1,19 +1,19 @@
 class Materialized < Formula
   desc "Streaming SQL database powered by Timely Dataflow"
   homepage "https://materialize.io/docs/"
-  url "https://github.com/MaterializeInc/materialize/archive/v0.7.3.tar.gz"
-  sha256 "228a6e5312eba04133e26427bde6cf30c2694cbae661dfaadc743c8e62ac0aa5"
+  url "https://github.com/MaterializeInc/materialize/archive/v0.8.0.tar.gz"
+  sha256 "7dc168b401ed72c8016b78b6fdec1d86405e068dd6475ca5f83f0b2f617defec"
   head "https://github.com/MaterializeInc/materialize.git", branch: "main"
 
   bottle do
     root_url "https://packages.materialize.io/homebrew"
-    sha256 high_sierra: "9f3fdce695c1a0ff5ef8f45e631ed986eb88cf0c16a4f2b83c742f6c7faed8eb"
+    sha256 high_sierra: "392ac90edc281b47b0d3f5c45999a1495a0a87343144088520fc03f482363848"
   end
 
   depends_on "cmake" => :build
   depends_on "rust" => :build
 
-  STABLE_BUILD_SHA = "c186b24463b5ce4d8ef70cc0bf9c645bb7c3d1b2".freeze
+  STABLE_BUILD_SHA = "b2fe225f1afcfec4912976bdaa4a44caf3ca0842".freeze
 
   def build_sha
     if head?
