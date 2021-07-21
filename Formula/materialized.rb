7a7847e9250d4dac4f88884a2113e0b9a73e4557
Brandon W Maister <bwm@materialize.io>
2021-07-21 09:47:12 -0400

    v0.8.3

--- Formula/materialized.rb
+++ Formula/materialized.rb
@@ -1,19 +1,19 @@
 class Materialized < Formula
   desc "Streaming SQL database powered by Timely Dataflow"
   homepage "https://materialize.io/docs/"
-  url "https://github.com/MaterializeInc/materialize/archive/v0.8.2.tar.gz"
-  sha256 "61fab3c1c6323add52916181a5edc70970a4832e3f79558b2cc35535285e57d0"
+  url "https://github.com/MaterializeInc/materialize/archive/v0.8.3.tar.gz"
+  sha256 "c63f3b011318b9ce90693e7e930d8059f1d613a209ca9ae4613c4bbe6857b737"
   head "https://github.com/MaterializeInc/materialize.git", branch: "main"
 
   bottle do
     root_url "http://homebrew.materialize.com"
-    sha256 high_sierra: "3491520271e9e90dd748b98f960f866666cf2b1c14c02d362bf00880b27705b9"
+    sha256 high_sierra: "cf8489bede9692c2bcc53d9292d57955cc7167c28c08a0e7bbcef196d7f03b9b"
   end
 
   depends_on "cmake" => :build
   depends_on "rust" => :build
 
-  STABLE_BUILD_SHA = "c40f601d62b891d85694cd188b2e6a90a6d21208".freeze
+  STABLE_BUILD_SHA = "3e47d280945f0ddbdc263fc643f067649f66eda1".freeze
 
   def build_sha
     if head?
