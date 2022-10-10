8c6622a42c98ccfc0b8dcc61716b5d546c5c2983
Joaquin Colacci <joaquincolacci@gmail.com>
2022-10-10 11:05:05 -0300

    fix: update formula version

--- Formula/mz.rb
+++ Formula/mz.rb
@@ -2,7 +2,7 @@ class Mz < Formula
     version '0.0.0'
     desc "Command-line interface (CLI) for Materialize."
     homepage "https://materialize.io/docs/"
-    url "https://github.com/MaterializeInc/materialize/archive/refs/tags/v0.27.0-alpha.22.tar.gz"
+    url "https://github.com/MaterializeInc/materialize/archive/refs/tags/v0.27.0-alpha.26.tar.gz"
     sha256 "5cc8bcbe2643fc48a08f853e13efe005c3f9f357b66c51ac428be6271d896500"
     head "https://github.com/MaterializeInc/materialize.git", branch: "main"
 
