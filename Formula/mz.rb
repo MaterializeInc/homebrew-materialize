bf295c04b2ed94f07ff25fd60b26751206eadd57
Nikhil Benesch <nikhil.benesch@gmail.com>
2023-03-19 17:53:44 -0400

    Update mz SHAs

--- Formula/mz.rb
+++ Formula/mz.rb
@@ -9,7 +9,7 @@ class Mz < Formula
 
   if Hardware::CPU.arm?
     url "https://binaries.materialize.com/mz-v#{VERSION}-aarch64-apple-darwin.tar.gz"
-    sha256 "e8cd094f24b3014b98cb21072e84263c820d3e03079498ecfa3f3fa403b18e8e"
+    sha256 "945f63b3514279f334fca35d1ea078d71354d2ae57da42fe57af86c6d0866167"
 
     def install
       bin.install "bin/mz"
@@ -17,7 +17,7 @@ class Mz < Formula
   end
   if Hardware::CPU.intel?
     url "https://binaries.materialize.com/mz-v#{VERSION}-x86_64-apple-darwin.tar.gz"
-    sha256 "676b70c3b071f3d183900cba694a30bf8351596e556b91002117b14cf4b6fc4f"
+    sha256 "06be949147015551657092d0da9a95fafeee6a3e4e87fd1a7fecc124c0646133"
 
     def install
       bin.install "bin/mz"
