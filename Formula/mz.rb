444237a1b738c50c8ab6744a7f528093508f7237
Joaquin Colacci <joaquincolacci@gmail.com>
2023-08-18 17:31:45 +0200

    Update mz.rb
    `mz` release for v0.1.3 - includes the new refactoring code.

--- Formula/mz.rb
+++ Formula/mz.rb
@@ -5,11 +5,11 @@ class Mz < Formula
 
   depends_on "postgresql@14"
 
-  VERSION = "0.1.2"
+  VERSION = "0.1.3"
 
   if Hardware::CPU.arm?
     url "https://binaries.materialize.com/mz-v#{VERSION}-aarch64-apple-darwin.tar.gz"
-    sha256 "670c1827f9eca5349487af84247040e84c5148ebebcab0d40da3bb932f7b2899"
+    sha256 "907bd4362e86fbbeb0640e96bf5fa008427ba06e0fffc61b788a628c4f49c0fd"
 
     def install
       bin.install "bin/mz"
@@ -17,7 +17,7 @@ class Mz < Formula
   end
   if Hardware::CPU.intel?
     url "https://binaries.materialize.com/mz-v#{VERSION}-x86_64-apple-darwin.tar.gz"
-    sha256 "6c3880c7c7bca8d51b8e434614fa1f1a627d2853645ddb67b4f2b1c0600fb46d"
+    sha256 "fb527e49ebb15e2c87d61ab4d00fcf290c625c082290e85a38ef3ee98aaa27ef"
 
     def install
       bin.install "bin/mz"
